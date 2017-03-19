dad5659776dec7f8e3e8f60de8c0676904dd55a2
Yu-Heng Lei <yuhengl1@andrew.cmu.edu>
2017-03-19 18:33:30 -0400

    PCFG takes an grammar file and output top_K sentences with descending probabilities instead
    Usage: python3 pcfg.py grammar_file top_k

--- pcfg.py
+++ pcfg.py
@@ -1,15 +1,23 @@
+# NLTK original code: http://www.nltk.org/_modules/nltk/parse/generate.html
+
 from __future__ import print_function
 
 import sys
 import itertools
 from nltk.parse.generate import generate, demo_grammar
-from nltk.grammar import Nonterminal
-from nltk import CFG, PCFG
-from nltk.grammar import toy_pcfg2
+from nltk.grammar import Nonterminal, CFG, PCFG, toy_pcfg2
 from nltk.probability import DictionaryProbDist
+import numpy as np
 
+# Global variables for individual probabilities of sentence and production
+# Collect the sentence probabilities in global list prob_all
+# Ryan: bp_debug = True indicates where I set a breakpoint
+prob_sent = 1.0
+prob_prod = 1.0
+prob_all = []
 
-def my_generate(grammar, start=None, depth=None, n=None):
+# original function offered by NLTK
+def generate(grammar, start=None, depth=None, n=None):
     """
     Generates an iterator of all sentences from a CFG.
 
@@ -24,18 +32,20 @@ def my_generate(grammar, start=None, depth=None, n=None):
     if depth is None:
         depth = sys.maxsize
 
-    iter = _generate_all(grammar, [start], depth)
+    # iter = _generate_all(grammar, [start], depth)
+    iter = generate_all_prob(grammar, [start], depth)
 
     if n:
         iter = itertools.islice(iter, n)
 
     return iter
     
-# original function offered by NLTK, reuse
+# original function offered by NLTK
 def _generate_all(grammar, items, depth):
     if items:    
         try:
-            for frag1 in my_generate_one(grammar, items[0], depth):
+            # for frag1 in my_generate_one(grammar, items[0], depth):
+            for frag1 in _generate_one(grammar, items[0], depth):
                 for frag2 in _generate_all(grammar, items[1:], depth):
                     yield frag1 + frag2
         except RuntimeError as _error:
@@ -47,6 +57,32 @@ def _generate_all(grammar, items, depth):
     else:
         yield []
 
+# Ryan: Collect sentence probability when the recursion yields a whole sentence
+def generate_all_prob(grammar, items, depth):
+    if items:
+        try:
+            # for frag1 in my_generate_one(grammar, items[0], depth):
+            for frag1 in generate_one_prob(grammar, items[0], depth):
+                for frag2 in generate_all_prob(grammar, items[1:], depth):
+                    yield frag1 + frag2
+                    # When items == start symbol, it will yield a whole sentence
+                    if len(items) == 1 and items[0] == grammar.start():
+                        global prob_sent, prob_all
+                        debug_p = prob_sent
+                        debug_bp = True
+                        prob_all.append(prob_sent)
+
+        except RuntimeError as _error:
+            if _error.message == "maximum recursion depth exceeded":
+                # Helpful error message while still showing the recursion stack.
+                raise RuntimeError("The grammar has rule(s) that yield infinite recursion!!")
+            else:
+                # Ryan: Print the exception
+                print(_error.message)
+    else:
+        yield []
+
+
 # original function offered by NLTK
 def _generate_one(grammar, item, depth):
     if depth > 0:
@@ -57,7 +93,32 @@ def _generate_one(grammar, item, depth):
         else:
             yield [item]
 
-def my_generate_one(grammar, item, depth):
+# Ryan: Multiply by the current production probability, and divide by it after the production is returned
+def generate_one_prob(grammar, item, depth):
+    if depth > 0:
+        if isinstance(item, Nonterminal):
+            for prod in grammar.productions(lhs=item):
+                # Multiply by current production probability when it is used
+                global prob_sent, prob_prod
+                prob_prod = prod.prob()
+                prob_sent *= prob_prod
+                debug_p = prob_sent
+                debug_bp = True
+
+                for frag in generate_all_prob(grammar, prod.rhs(), depth-1):
+                    yield frag
+
+                # Divide by current production probability after it is returned
+                prob_prod = prod.prob()
+                prob_sent /= prob_prod
+                debug_p = prob_sent
+                debug_bp = True
+        else:
+            yield [item]
+
+# Minxing: Randomly sample a sentence according to probabilities
+# Reference: https://www.cs.bgu.ac.il/~elhadad/nlp11/hw2-11.html#q1
+def generate_one_sample(grammar, item, depth):
     if depth > 0:
         if isinstance(item, Nonterminal):
         
@@ -77,22 +138,34 @@ def my_generate_one(grammar, item, depth):
         else:
             yield [item]
 
-print(toy_pcfg2)
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: %s grammar_file top_K' % sys.argv[0])
+        print('Example: %s grammar/toy_pcfg2.txt 20' % sys.argv[0])
+        exit(0)
+    grammar_txt = open(sys.argv[1]).read()
+    top_K = int(sys.argv[2])
+    print('Grammar:\n' + grammar_txt + '\n')
+    grammar = PCFG.fromstring(grammar_txt)
 
-# a grammer can be generated from a string, toy_pcfg2 is a generated grammer
-my_grammer = PCFG.fromstring("""
-    S -> NP VP [1.0] \n
-    NP -> Det N [0.5] | NP PP [0.25] | 'John' [0.1] | 'I' [0.15] \n
-    Det -> 'the' [0.8] | 'my' [0.2] \n
-    N -> 'man' [0.5] | 'telescope' [0.5] \n
-    VP -> VP PP [0.1] | V NP [0.7] | V [0.2] \n
-    V -> 'ate' [0.35] | 'saw' [0.65] \n
-    PP -> P NP [1.0] \n
-    P -> 'with' [0.61] | 'under' [0.39]""")
+    # print(toy_pcfg2)
 
-#productions = toy_pcfg2.productions()
-#np_productions = toy_pcfg2.productions(Nonterminal('NP'))
-#for pr in np_productions:
-#    print(pr.rhs())
-for sentence in my_generate(toy_pcfg2, n=10):
-    print(' '.join(sentence))
+    #productions = toy_pcfg2.productions()
+    #np_productions = toy_pcfg2.productions(Nonterminal('NP'))
+    #for pr in np_productions:
+    #    print(pr.rhs())
+
+    # Need to plus one to mysteriously workaround for prob_all in recursion
+    generator = generate(grammar, n=top_K+1)
+    sentences = []
+
+    # Collect the n generated sentences
+    for sent in generator:
+        sentence = ' '.join(sent)
+        sentences.append(sentence)
+    # # Sort the sentences by descending probability
+    sorted_idx = np.argsort(prob_all)[::-1]
+
+    print('Top %d Sentences:' % top_K)
+    for rank, idx in enumerate(sorted_idx, 1):
+        print('%03d [%e]: %s' % (rank, prob_all[idx], sentences[idx]))
