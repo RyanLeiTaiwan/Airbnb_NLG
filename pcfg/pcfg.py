5abf6d05af173cf607e5b286910f7b13a6b22ffc
Yu-Heng Lei <ryanlei750328@gmail.com>
2017-03-27 14:30:37 -0400

    Basic PCFG now replaces [brackets] with CSV values
    Usage: pcfg.py grammar_file csv_file skiprows nrows (-1 for all) top_K
    Example: pcfg.py grammar/airbnb_dynamic.txt ../data/Airbnb/SanFrancisco_details.csv 0 3 5

--- pcfg/pcfg.py
+++ pcfg/pcfg.py
@@ -4,10 +4,10 @@ from __future__ import print_function
 
 import sys
 import itertools
-from nltk.parse.generate import generate, demo_grammar
-from nltk.grammar import Nonterminal, CFG, PCFG, toy_pcfg2
+from nltk.grammar import Nonterminal, PCFG
 from nltk.probability import DictionaryProbDist
 import numpy as np
+import pandas as pd
 
 # Global variables for individual probabilities of sentence and production
 # Collect the sentence probabilities in global list prob_all
@@ -15,6 +15,7 @@ import numpy as np
 prob_sent = 1.0
 prob_prod = 1.0
 prob_all = []
+
 
 # original function offered by NLTK
 def generate(grammar, start=None, depth=None, n=None):
@@ -39,10 +40,11 @@ def generate(grammar, start=None, depth=None, n=None):
         iter = itertools.islice(iter, n)
 
     return iter
-    
+
+
 # original function offered by NLTK
 def _generate_all(grammar, items, depth):
-    if items:    
+    if items:
         try:
             # for frag1 in my_generate_one(grammar, items[0], depth):
             for frag1 in _generate_one(grammar, items[0], depth):
@@ -56,6 +58,7 @@ def _generate_all(grammar, items, depth):
                 raise
     else:
         yield []
+
 
 # Ryan: Collect sentence probability when the recursion yields a whole sentence
 def generate_all_prob(grammar, items, depth):
@@ -88,10 +91,11 @@ def _generate_one(grammar, item, depth):
     if depth > 0:
         if isinstance(item, Nonterminal):
             for prod in grammar.productions(lhs=item):
-                for frag in _generate_all(grammar, prod.rhs(), depth-1):
+                for frag in _generate_all(grammar, prod.rhs(), depth - 1):
                     yield frag
         else:
             yield [item]
+
 
 # Ryan: Multiply by the current production probability, and divide by it after the production is returned
 def generate_one_prob(grammar, item, depth):
@@ -105,7 +109,7 @@ def generate_one_prob(grammar, item, depth):
                 debug_p = prob_sent
                 debug_bp = True
 
-                for frag in generate_all_prob(grammar, prod.rhs(), depth-1):
+                for frag in generate_all_prob(grammar, prod.rhs(), depth - 1):
                     yield frag
 
                 # Divide by current production probability after it is returned
@@ -116,12 +120,13 @@ def generate_one_prob(grammar, item, depth):
         else:
             yield [item]
 
+
 # Minxing: Randomly sample a sentence according to probabilities
 # Reference: https://www.cs.bgu.ac.il/~elhadad/nlp11/hw2-11.html#q1
 def generate_one_sample(grammar, item, depth):
     if depth > 0:
         if isinstance(item, Nonterminal):
-        
+
             # get all relations starting with item
             np_productions = grammar.productions(item)
             dict = {}
@@ -129,36 +134,76 @@ def generate_one_sample(grammar, item, depth):
             for pr in np_productions:
                 dict[pr.rhs()] = pr.prob()
             np_probDist = DictionaryProbDist(dict)
-            
+
             # np_probDist.generate() samples a probable expansion
-            # in contrast to the iterative 
+            # in contrast to the iterative
             for prod in grammar.productions(lhs=item):
-                for frag in _generate_all(grammar, np_probDist.generate(), depth-1):
+                for frag in _generate_all(grammar, np_probDist.generate(), depth - 1):
                     yield frag
         else:
             yield [item]
 
+# Replace the string inside (non-nested) brackets with CSV (row) values
+# http://stackoverflow.com/questions/4894069/regular-expression-to-return-text-between-parenthesis
+def replace_brackets(sent, row):
+    ret = ''
+    start = 0
+    left = sent.find('[', start)
+    while left != -1:
+        # Found left bracket
+        ret += sent[start:left]
+        right = sent.find(']', start)
+        if right == -1:
+            raise Exception('replace_brackets() error: cannot find matching right bracket')
+        else:
+            # Found string between a bracket
+            col_name = sent[left + 1: right]
+            # print(col_name)
+            if col_name in row.keys():
+                # Replace the string with CSV values.
+                # print(row[col_name])
+                ret += str(row.at[col_name])
+            else:
+                # Don't replace if not found
+                ret += str('[' + col_name + ']')
+            start = right + 1
+            left = sent.find('[', start)
+    ret += sent[start:]
+    return ret
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: %s grammar_file top_K' % sys.argv[0])
-        print('Example: %s grammar/toy_pcfg2.txt 20' % sys.argv[0])
+    # skiprows: number of lines to skip at the start of the file
+    # nrows: number of rows of file to read.
+    # top_K: number of top probability sentences to generate for each property
+    if len(sys.argv) != 6:
+        print('Usage: %s grammar_file csv_file skiprows nrows (-1 for all) top_K' % sys.argv[0])
+        print('Example: %s grammar/airbnb_dynamic.txt ../data/Airbnb/SanFrancisco_details.csv 0 3 20' % sys.argv[0])
         exit(0)
     grammar_txt = open(sys.argv[1]).read()
-    top_K = int(sys.argv[2])
-    print('Grammar:\n' + grammar_txt + '\n')
+    csv_file = sys.argv[2]
+    skiprows = int(sys.argv[3])
+    nrows = int(sys.argv[4])
+    if nrows == -1:
+        nrows = None
+    top_K = int(sys.argv[5])
+
+    # print('Grammar:\n' + grammar_txt + '\n')
     grammar = PCFG.fromstring(grammar_txt)
 
-    # print(toy_pcfg2)
-
-    #productions = toy_pcfg2.productions()
-    #np_productions = toy_pcfg2.productions(Nonterminal('NP'))
-    #for pr in np_productions:
-    #    print(pr.rhs())
+    # Read CSV file into Pandas DataFrame
+    # Handle DtypeWarning: Columns (43) have mixed types. [One entry with zipcode '94107-1273']
+    # Fix dollar sign and thousands separators in 'price'
+    df = pd.read_csv(csv_file, skiprows=skiprows, nrows=nrows,
+                     dtype={'host_id': np.str, 'zipcode': np.str},
+                     converters={'price': lambda s: float(s.replace('$', '').replace(',', ''))})
+    # Select only the interested columns
+    df = df.loc[:, ['id', 'accommodates', 'bedrooms', 'bathrooms', 'square_feet', 'property_type',
+                    'city', 'neighbourhood_cleansed']]
 
     # Set a "large enough" fixed max_generated so that recursion won't raise an exception
     max_generated = 2000
     # Need to plus one to mysteriously workaround for prob_all in recursion
-    generator = generate(grammar, n=max_generated+1)
+    generator = generate(grammar, n=max_generated + 1)
     sentences = []
 
     # Collect the n generated sentences
@@ -168,6 +213,12 @@ if __name__ == '__main__':
     # Sort the sentences by descending probability
     sorted_idx = np.argsort(prob_all)[::-1]
 
-    print('Top %d Sentences:' % top_K)
-    for rank, idx in enumerate(sorted_idx[:top_K], 1):
-        print('%03d [%e]: %s' % (rank, prob_all[idx], sentences[idx]))
+    # Post processing: for each row (property), replace brackets in each generated sentence
+    #   with the corresponding CSV values
+    for idx_df, row in df.iterrows():
+        print('id: ' + str(row.at['id']))
+        for rank, idx_rank in enumerate(sorted_idx[:top_K], 1):
+            sent_output = replace_brackets(sentences[idx_rank], row)
+            print('%03d [%e]: %s' % (rank, prob_all[idx_rank], sent_output))
+        # Extra newline
+        print('')
