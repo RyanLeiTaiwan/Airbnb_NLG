7faa22718c9ebae04d33464abba9d254e87a46d4
Shuxin Lin <linshuhsin@gmail.com>
2017-04-10 12:01:37 -0400

    bleu

---
+++ matrics/bleu.py
@@ -0,0 +1,29 @@
+import nltk
+import sys
+from nltk import word_tokenize
+from nltk.translate.bleu_score import corpus_bleu
+
+candidate_file = sys.argv[1]	# auto-generated
+ref_file = sys.argv[2]	# original Airbnb dataset
+
+candidates = open(candidate_file)
+refs = open(ref_file)
+list_of_candidates = []
+list_of_references = []
+
+cnt = 0
+for candi_raw in candidates:
+	candi_raw = word_tokenize(candi_raw)
+	list_of_candidates.append(candi_raw)
+	cnt += 1
+
+for ref_raw in refs:
+	ref_raw = word_tokenize(ref_raw)
+	list_of_references.append(ref_raw)
+
+lists_of_references = [list_of_references] * cnt
+
+print corpus_bleu(lists_of_references, list_of_candidates)
+
+candidates.close()
+refs.close()
