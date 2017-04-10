165490c86edf255d5344426833b49b60789810e1
Yu-Heng Lei <ryanlei750328@gmail.com>
2017-04-10 16:11:26 -0400

    Skeleton to compute walking/driving time to attractions

--- pcfg/google_api.py
+++ pcfg/google_api.py
@@ -16,7 +16,9 @@ def distance_attractions(lat, lng, att, max_walk):
 		return 'unknown'
 
 if __name__ == '__main__':
+	lat = 42.28451220982457
+	lng = -71.1362580468337
 	att = ['roslindale village', 'roslindale square', 'the arnold arboretum', 'emerald necklace']
-	att = ['jp, the arnold arboretum, jamaica, jamaica pond']
-	att = ['the museum of fine arts', 'longwood', 'northeastern university', 'penguin pizza']
-	print distance_attractions(att=att, max_walk=60)
+	# att = ['jp, the arnold arboretum, jamaica, jamaica pond']
+	# att = ['the museum of fine arts', 'longwood', 'northeastern university', 'penguin pizza']
+	print distance_attractions(lat=lat, lng=lng, att=att, max_walk=60)
