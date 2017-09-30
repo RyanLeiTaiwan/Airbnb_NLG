7059ea668e49ec268b237f4dbc8e1361ef7851d6
ngekakis <nikogekakis@gmail.com>
2017-09-30 00:11:58 -0400

    Updated preprocessing logic to add white space to input sequence and generate a vocabulary list.

--- preprocess.py
+++ preprocess.py
@@ -1,30 +1,52 @@
 import pandas as pd
 import sys, os
 
-CITY_FILE = sys.argv[1]
+def incr(dic, key):
+    if key in dic:
+        dic[key] += 1
+    else:
+        dic[key] = 1
+
+CITY_DIR = sys.argv[1]
 COLUMNS = sys.argv[2]
+MIN_COUNT = int(sys.argv[3])
 
 cols = []
-
 col_file = open(COLUMNS, 'r')
+vocab = {}
 
 for line in col_file:
     cols.append(line.strip())
 
 col_file.close()
 
-file_name = CITY_FILE.split('/')[1]
+for fil in os.listdir(CITY_DIR):
+    new_file = fil.split('.')[0]
+    data_file = open("processed/" + new_file + '.data', 'w')
+    desc_file = open("processed/" + new_file + '.desc', 'w')
+    df = pd.read_csv(CITY_DIR + '/' + fil, header=0)
+    for idx in range(0, df.shape[0]):
+        output = []
+        description = str(df.loc[idx, 'description'])
+        if len(description) > 5:
+            for word in description:
+                incr(vocab, word)
+            for label in cols:
+                incr(vocab, word)
+                output.append(label)
+                info = str(df.loc[idx, label]).replace('\n', '')
+                for word in info.split(' '):
+                    incr(vocab, word)
+                output.append(info)
+            data_file.write(" , ".join(output) + '\n')
+            description = " ".join(description.split())
+            desc_file.write(description + '\n')
+    data_file.close()
+    desc_file.close()
+    print "Finished file: " + fil
 
-new_file = file_name.split('.')[0]
-data_file = open("processed/" + new_file + '.data', 'w')
-desc_file = open("processed/" + new_file + '.desc', 'w')
-df = pd.read_csv(CITY_FILE, header=0)
-for idx in range(0, df.shape[0]):
-    output = []
-    for label in cols:
-        output.append(label)
-        output.append(str(df.loc[idx, label]))
-    data_file.write(",".join(output) + '\n')
-    desc_file.write(str(df.loc[idx, 'description']) + '\n')
-data_file.close()
-desc_file.close()
+v_file = open('vocab.txt', 'w')
+for x in vocab:
+    if vocab[x] >= MIN_COUNT:
+        v_file.write(x + '\n')
+v_file.close()
