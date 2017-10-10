e2a4860137be7a66c0b0db1f156534297a20012d
ngekakis <nikogekakis@gmail.com>
2017-10-10 17:16:00 -0400

    Updated preprocessing to working version

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import sys, os
+import argparse
+import string, re
+from langdetect import detect
 
 def incr(dic, key):
     if key in dic:
@@ -7,46 +10,132 @@ def incr(dic, key):
     else:
         dic[key] = 1
 
-CITY_DIR = sys.argv[1]
-COLUMNS = sys.argv[2]
-MIN_COUNT = int(sys.argv[3])
+def handle_amenities(amens):
+    amens_rgx = re.compile('[^0-9a-zA-Z ]+')
+    amens = (amens.replace(",", " ")).lower()
+    amens = re.sub(amens_rgx, "", amens)
+    return amens
 
-cols = []
-col_file = open(COLUMNS, 'r')
-vocab = {}
-
-for line in col_file:
-    cols.append(line.strip())
-
-col_file.close()
-
-for fil in os.listdir(CITY_DIR):
-    new_file = fil.split('.')[0]
+def process(city_dir, file_name, columns, vocab_flag):
+    global vocabulary, line_errs
+    new_file = file_name.split('.')[0]
     data_file = open("processed/" + new_file + '.data', 'w')
     desc_file = open("processed/" + new_file + '.desc', 'w')
-    df = pd.read_csv(CITY_DIR + '/' + fil, header=0)
+    df = pd.read_csv(city_dir + '/' + fil, header=0)
     for idx in range(0, df.shape[0]):
-        output = []
+        data_output = []
+        desc_output = []
         description = str(df.loc[idx, 'description'])
-        if len(description) > 5:
-            for word in description:
-                incr(vocab, word)
-            for label in cols:
-                incr(vocab, word)
-                output.append(label)
-                info = str(df.loc[idx, label]).replace('\n', '')
-                for word in info.split(' '):
-                    incr(vocab, word)
-                output.append(info)
-            data_file.write(" , ".join(output) + '\n')
-            description = " ".join(description.split())
-            desc_file.write(description + '\n')
+        try:
+            if len(description) > 5:
+                if detect(description) == 'en':
+                    for word in description.split():
+                        word = word.lower()
+                        if word[0] in string.punctuation:
+                            start = word[0]
+                            incr(vocabulary, start)
+                            desc_output.append(start)
+                            word = word[1:]
+                        if word[-1:] in string.punctuation:
+                            end = word[-1:]
+                            word = word[:-1]
+                            incr(vocabulary, word)
+                            desc_output.append(word)
+                            incr(vocabulary, end)
+                            desc_output.append(end)
+                        else:
+                            incr(vocabulary, word)
+                            desc_output.append(word)
+                    for label in cols:
+                        if label == 'amenities':
+                            info = handle_amenities(str(df.loc[idx, label]).replace('\n', ''))
+                        else:
+                            info = str(df.loc[idx, label]).replace('\n', '')
+                        incr(vocabulary, label)
+                        data_output.append(label)
+                        build_string = []
+                        for word in info.split(' '):
+                            word = word.lower()
+                            if word[0] in string.punctuation:
+                                start = word[0]
+                                incr(vocabulary, start)
+                                build_string.append(start)
+                                word = word[1:]
+                            if word[-1:] in string.punctuation:
+                                end = word[-1:]
+                                word = word[:-1]
+                                incr(vocabulary, word)
+                                build_string.append(word)
+                                incr(vocabulary, end)
+                                build_string.append(end)
+                            else:
+                                incr(vocabulary, word)
+                                build_string.append(word)
+                        data_output.append(" ".join(build_string))
+                    data_file.write(" , ".join(data_output) + '\n')
+                    description = " ".join(desc_output)
+                    desc_file.write(re.sub("\s+"," ",description) + '\n')
+        except:
+            line_errs += 1
     data_file.close()
     desc_file.close()
-    print "Finished file: " + fil
+    print "Finished file: " + file_name
 
-v_file = open('vocab.txt', 'w')
-for x in vocab:
-    if vocab[x] >= MIN_COUNT:
-        v_file.write(x + '\n')
-v_file.close()
+def build_parser():
+    parser = argparse.ArgumentParser(description='Preprocess AirBnB CSV files.')
+    parser.add_argument(
+        '-p', '--csv_path',
+        default='.',
+        help='The path to the directory containing the unprocessed CSV files. Default: current directory.'
+    )
+    parser.add_argument(
+        '-c', '--column_path',
+        required=True,
+        help='The path to the file containing the columns to include. Required.'
+    )
+    parser.add_argument(
+        '-v', '--vocab',
+        action='store_false',
+        default=True,
+        help='Flag if no new vocabulary file should be produced. Omit to create a new vocabulary file.'
+    )
+    parser.add_argument(
+        '-m', '--minimum',
+        default=1,
+        type=int,
+        help='Integer value determining minimum count for token to be included in vocabulary. Default: 1.'
+    )
+    return parser
+
+def get_cols(file_name):
+    if os.path.isfile(file_name):
+        cols = []
+        col_file = open(file_name, 'r')
+        for line in col_file:
+            cols.append(line.strip())
+        col_file.close()
+        return cols
+    else:
+        print "ERROR - File not found at: " + file_name + ". Please provide valid column file."
+        sys.exit()
+
+if __name__ == '__main__':
+    parser = build_parser()
+    n = parser.parse_args()
+
+    global vocabulary, line_errs
+    vocabulary = {}
+    line_errs = 0
+    cols = get_cols(n.column_path)
+
+    for fil in os.listdir(n.csv_path):
+        process(n.csv_path, fil, cols, n.vocab)
+
+    if n.vocab:
+        v_file = open('vocab.txt', 'w')
+        for x in vocabulary:
+            if vocabulary[x] >= n.minimum:
+                v_file.write(x + '\n')
+        v_file.close()
+
+    print "Finished with " + str(line_errs) + " skipped lines."
